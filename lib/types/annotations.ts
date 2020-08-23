ea0852540bdd719b89c80702824e0198a6650e52
Yiğit Yüce <ygtyce@gmail.com>
2020-08-23 16:15:34 +0300

    added proprty annotation type

--- lib/types/annotations.ts
+++ lib/types/annotations.ts
@@ -2,4 +2,5 @@ import { Constructor } from './base';
 
 export type ClassAnnotationReturnType = <T extends Constructor>(ctor: T) => T;
 export type MethodAnnotationReturnType = (src: any, fname: string, descriptor: PropertyDescriptor) => PropertyDescriptor;
+export type PropertyAnnotationReturnType = (src: any, pname: string) => void;
 export type ParameterAnnotationReturnType = (src: any, propertyKey: string, index: number) => void;
